8b082614e24fc0ffb7333115e96e01dc2ee14988
Sumson90 <Sumkowski.Tomasz@gmail.com>
2023-03-17 12:47:08 +0100

    01 Main page layout project

---
+++ src/main/java/pl/MyFavoriteRecipeApplication.java
@@ -0,0 +1,13 @@
+package pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFavoriteRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFavoriteRecipeApplication.class, args);
+	}
+
+}
