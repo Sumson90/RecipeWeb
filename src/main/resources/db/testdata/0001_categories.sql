c6da1c18b9f26ed83dfd69c479d0712cc131035d
Sumson90 <Sumkowski.Tomasz@gmail.com>
2023-03-17 17:01:29 +0100

    05 Prepared test data with categories and recipes

---
+++ src/main/resources/db/testdata/0001_categories.sql
@@ -0,0 +1,10 @@
+insert into category (name) values ('Vege');
+insert into category (name) values ('Meat');
+insert into category (name) values ('Asian');
+insert into category (name) values ('Italy');
+insert into category (name) values ('Polish');
+insert into category (name) values ('Hungary');
+insert into category (name) values ('Greece');
+insert into category (name) values ('France');
+insert into category (name) values ('Mexico');
+insert into category (name) values ('Thai');
