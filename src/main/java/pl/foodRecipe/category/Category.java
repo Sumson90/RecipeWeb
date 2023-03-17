cf7a680a0af93d9d056cbca4f093cf6ff4bc3f7a
Sumson90 <Sumkowski.Tomasz@gmail.com>
2023-03-17 16:41:47 +0100

    04 Setted h2 console and created data base structure

---
+++ src/main/java/pl/foodRecipe/category/Category.java
@@ -0,0 +1,22 @@
+package pl.foodRecipe.category;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+}
---
+++ src/main/java/pl/foodRecipe/movie/Recipe.java
@@ -0,0 +1,26 @@
+package pl.foodRecipe.movie;
+
+import lombok.*;
+import pl.foodRecipe.category.Category;
+
+import javax.persistence.*;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+    private boolean promoted;
+
+
+}
